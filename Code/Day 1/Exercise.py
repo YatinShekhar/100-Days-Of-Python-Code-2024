4479c17863cb1a7c2a2b4e191ee44e71e65c48eb
Yatin Shekhar Rikhari <121398971+YatinShekhar@users.noreply.github.com>
2024-06-16 12:56:44 +0530

    Update and rename untitled0.py to Exercise.py

--- Code/Day 1/Exercise.py
+++ Code/Day 1/Exercise.py
@@ -6,6 +6,7 @@ print("""1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
 4. Leave to rise for 2 hours.
 5. Bake at 200 degrees C for 30 minutes.""")
 
+
 # Exercise 2: Debugging Practice
 
 print("Day 1 - String Manipulation")
@@ -13,9 +14,11 @@ print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
+
 # Exercise 3: Input Function
 
 print(len(input()))
+
 
 # Exercise 4: Variables
 
