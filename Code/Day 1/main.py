f759d3e2cc7125d1e5539b9f8648b1a30dff7816
Yatin Shekhar Rikhari <121398971+YatinShekhar@users.noreply.github.com>
2024-06-16 12:58:34 +0530

    Create main.py

---
+++ Code/Day 1/main.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator!")
+
+print("What is the city name you grew up in?")
+city = input()
+
+print("What is your pet's name?")
+pet = input()
+
+print(f"Your band name could be {city} {pet}.")
